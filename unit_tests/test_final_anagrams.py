9cac7956ef6a4b5ae8f46abeb56ba7ad9336a767
arpitsaxena <arpitsaxena239@gmail.com>
2020-08-30 23:55:50 -0500

    Implemented Anagram pytest cases 2.0

--- unit_tests/test_final_anagrams.py
+++ unit_tests/test_final_anagrams.py
@@ -1,10 +1,14 @@
 import unittest
+import datetime
+from find_anagrams import start_time_method, execution_time_method
 
+class TestStringSorter(unittest.TestCase):
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_string_sorter(self):
+        self.assertLessEqual(start_time_method(), datetime.datetime.now())
 
+    def test_sorted_list_of_strings(self):
+        self.assertGreaterEqual(execution_time_method(datetime.datetime.now()), 0)
 
 if __name__ == '__main__':
     unittest.main()
